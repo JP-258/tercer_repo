4e79bc1ce18ef3c35cdab7a80eaa63a2912fa146
Jean Pool Arias <Jean-Pool@gft.com>
2025-02-27 16:28:23 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TERCER_REPO",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Jean Arias",                         # Tu nombre
+    author_email="amin@platzi.com",                 # Tu correo electrónico
+    url="https://github.com/JP-258/git-github",     # URL del proyecto
+)
